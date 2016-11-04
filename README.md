779422da1a30cff46c9f559590e98762263d6497
Rob Hess <robwhess@gmail.com>
2016-11-04 12:13:34 -0700

    Add complete server code.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# express-middleware-demo
-A small server demonstrating Express middleware
+# Express Middleware Demo Server
+
+This repo contains a basic Express-based server in `server.js` that demonstrates some of the ways middleware can be used in Express.  Before doing anything with it you need to install dependencies:
+```
+npm install
+```
+
+Then, you can start the server:
+```
+npm start
+```
+This will start the server running on port 3000 by default.  You can change the port it runs on by setting the `PORT` environment variable.  Once the server is running, you can visit it at http://localhost:3000.
+
+The server is also set up to handle (specifically to reject) HTTP POST requests.  You can test this, for example, in a Unix terminal, like this:
+```
+curl -X POST http://localhost:3000
+```
---
+++ server.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var logger = require('./lib/logger');
+var app = express();
+var port = process.env.PORT || 3000;
+
+/*
+ * This call to app.use() sets up our first middleware function.  Since this
+ * is the first call to app.use, this will be the first thing called for every
+ * request.
+ */
+app.use(function (req, res, next) {
+  console.log("\n== For every request, this line will be printed first.");
+  next();
+});
+
+/*
+ * Any request handling function can be used as middleware, even one imported
+ * from a module, like this one.
+ */
+app.use(logger);
+
+/*
+ * A middleware function can modify the request or response objects, like
+ * this one does.
+ */
+app.use(function (req, res, next) {
+  var now = new Date();
+  req.requestTime = now.toString();
+  next();
+});
+
+/*
+ * This middleware function will only be called for HTTP GET requests for the
+ * URL path '/'.  That's what app.get('/', ...) specifies.  There's no call to
+ * next() because we are ending the request-response cycle here (with
+ * req.send()).
+ */
+app.get('/', function (req, res) {
+
+  var content = "<html>";
+  content += "<body>";
+  content += "<h1>Welcome to this page!</h1>";
+  content += "<p>We hope you like it.</p>";
+  content += "<p>You made this request at " + req.requestTime + "</p>";
+  content += "</body>";
+  content += "</html>";
+
+  res.status(200);
+  res.send(content);
+
+});
+
+/*
+ * This middleware function will be used on all HTTP POST requests.  That's
+ * what app.post('*', specifies).
+ */
+app.post('*', function (req, res) {
+  // Send a 405 status to indicate that POSTS aren't allowed.
+  res.status(405);
+  res.send("No POSTs allowed");
+});
+
+/*
+ * This middleware function is an error handler.  If the app.get() function
+ * above can't be applied (because the requested URL didn't match '/'), then
+ * this function will be called and will send a 404 response.  If the user
+ * requested URL '/', this middleware function will not be reached because
+ * the middleware above will handle it (and end the request-response cycle)
+ * first.
+ */
+app.get('*', function (req, res) {
+
+  var content = "<html>";
+  content += "<body>";
+  content += "<h1>404 Error</h1>";
+  content += "<p>We couldn't find the page you asked for: " + req.url + "</p>";
+  content += "</body>";
+  content += "</html>";
+
+  res.status(404);
+  res.send(content);
+
+})
+
+// Start the server listening on the specified port.
+app.listen(port, function () {
+  console.log("== Server listening on port", port);
+});
